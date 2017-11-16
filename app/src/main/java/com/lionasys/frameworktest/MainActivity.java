d6f707c554810546cd130a5c08a74a5cb9c0b3bc
Urvi <urvi@lionasys.in>
2017-11-16 11:34:01 +0530

    added adMob to it.

--- app/src/main/java/com/lionasys/frameworktest/MainActivity.java
+++ app/src/main/java/com/lionasys/frameworktest/MainActivity.java
@@ -2,16 +2,47 @@ package com.lionasys.frameworktest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import com.lionasys.mylibrary.MyView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public AdView mAdView;
+    AdRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View v = new MyView(this);
-        setContentView(v);
+        // View v = new MyView(R.layout.my_view);
+        setContentView(R.layout.my_view);
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
 }
--- mylibrary/src/main/java/com/lionasys/mylibrary/MyView.java
+++ mylibrary/src/main/java/com/lionasys/mylibrary/MyView.java
@@ -1,18 +1,50 @@
 package com.lionasys.mylibrary;
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 /**
  * Created by urvi on 14/11/17.
  */
 
-public class MyView extends LinearLayout {
+public class MyView extends AppCompatActivity {
 
-    public MyView(Context context) {
+    public static AdView mAdViewBottom;
+    RelativeLayout relativeLayout;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_view);
+
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-3940256099942544~3347511713");
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+       /* mAdViewTop = findViewById(R.id.adViewTop);
+        AdRequest adRequestTop = new AdRequest.Builder().build();
+        mAdViewTop.loadAd(adRequestTop);*/
+
+        mAdViewBottom = findViewById(R.id.adView);
+        AdRequest adRequestButtom = new AdRequest.Builder().build();
+        mAdViewBottom.loadAd(adRequestButtom);
+
+
+
+
+    /*public MyView(Context context) {
         super(context);
         initialize(context);
+
+        AdView adView = new AdView(context);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
 
     }
 
@@ -24,5 +56,40 @@ public class MyView extends LinearLayout {
 
     private void initialize(Context context){
         inflate(context, R.layout.my_view, this);
+    }*/
+
     }
+
+    public void setAdTop(View view){
+
+        RelativeLayout.LayoutParams adViewParams = new RelativeLayout.LayoutParams(
+                AdView.LayoutParams.WRAP_CONTENT,
+                AdView.LayoutParams.WRAP_CONTENT);
+        // align top
+        adViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        // align center
+        adViewParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+
+        relativeLayout.addView(mAdViewBottom, adViewParams);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdViewBottom.loadAd(adRequest);
+    }
+
+    public void setAdBottom(View view){
+
+        RelativeLayout.LayoutParams adViewParams = new RelativeLayout.LayoutParams(
+                AdView.LayoutParams.WRAP_CONTENT,
+                AdView.LayoutParams.WRAP_CONTENT);
+        // align top
+        adViewParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        // align center
+        adViewParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+
+        relativeLayout.addView(mAdViewBottom, adViewParams);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdViewBottom.loadAd(adRequest);
+    }
+
 }
